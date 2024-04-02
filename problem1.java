fe4b7be495595a92c94144fde352921e9186619a
Mansur <155591937+Mnebezsaxara@users.noreply.github.com>
2024-04-02 11:19:19 +0600

    Create problem1.java

---
+++ problem1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class problem1 {
+    public static int min(int n, int[] arr) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("Array is empty");
+        }
+
+        int min = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int minEl = min(n, array);
+        System.out.println("Minimum element: " + minEl);
+
+        scanner.close();
+    }
+}

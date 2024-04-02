9adda00e472e669b7417e5732b572e851cd14ada
Mansur <155591937+Mnebezsaxara@users.noreply.github.com>
2024-04-02 11:25:29 +0600

    Create problem2.java

---
+++ problem2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class problem2 {
+    public static float average(int n, int[] arr) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("Array is empty");
+        }
+        float sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        return sum / n;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        float average = average(n, array);
+        System.out.println("Average is: "+ average);
+
+        scanner.close();
+    }
+}
